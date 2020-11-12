4dd7f8d079ee797d8caa1bb8921bd3de44b35521
JMarcos <jmarcos25eagle@gmail.com>
2020-11-11 19:14:48 -0600

    versión 1.0

---
+++ main.c
@@ -0,0 +1,42 @@
+/**
+ * @file main.c
+ * @version 1.0
+ * @date 04/11/2020
+ * @author J.Marcos
+ * @brief Solita los datos de las coordenadas
+ */
+
+#include "ruta.h"
+
+/**
+ * @brief Fichero principal
+ * @return 0
+ */
+int main(){
+	int i, aris, x[MAX], y[MAX];
+	int inicial, final, final2;
+	int rep;
+	printf("-------Ruta-------\n");
+	printf("Numero de aristas: ");
+	scanf("%d",&aris);
+	for(i=0; i<aris; i++){
+		printf("\nCordenadas de Adyacencia #%i\n",i+1);
+		printf("x: ");
+		scanf("%d",&x[i]);
+		printf("y: ");
+		scanf("%d",&y[i]);
+	}
+	imprimir_coordenadas(aris, x, y);
+	do{
+		printf("\nNodo inicial: ");
+		scanf("%d",&inicial);
+		printf("Nodo final: ");
+		scanf("%d",&final);
+		buscar_ruta(aris, x, y, inicial, final);
+		final2=buscar_ruta(aris, x, y, inicial, final);
+		existe_recorrido(inicial, final, final2);
+		printf("\n\nPresionar 1 si desea otro recorrido --> ");
+		scanf("%d",&rep);
+	}while(rep==1);
+	return 0;
+}
